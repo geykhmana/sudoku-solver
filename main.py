3ad6e830f7d85fb2b5ec55afed7fddbd4c57767b
geykhmana <alangeykhman@gmail.com>
2025-01-30 15:05:12 -0500

    Created main.py and uncached .gitignore

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import keras
+import keras.backend as K
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.utils import Sequence
+from keras.layers import *
