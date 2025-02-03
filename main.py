aabbb286d77761ee1a81fd78af1d8045323cdc14
geykhmana <alangeykhman@gmail.com>
2025-02-03 12:35:19 -0500

    Added code to read Pandas DataFrame, comments to ignore VSCode import warnings

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
-import keras
-import keras.backend as K
-from keras.optimizers import Adam
-from keras.models import Sequential
-from keras.utils import Sequence
-from keras.layers import *
+import tensorflow as tf
+from tensorflow.keras import backend as K # type: ignore
+from tensorflow.keras.optimizers import Adam # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.utils import Sequence # type: ignore
+from tensorflow.keras.layers import * # type: ignore
+
+data = pd.read_csv("/content/sudoku.csv")
+
+try:
+    data = pd.DataFrame({"quizzes": data["puzzle"], "solutions": data["solution"]})
+except:
+    pass
